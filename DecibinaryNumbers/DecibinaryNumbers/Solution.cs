06131a1599a7f92345a088fed81ec4933d5e6b40
PLDD <PLDD@DK01WV6074>
2018-05-06 23:06:06 +0200

    Some correction

--- DecibinaryNumbers/DecibinaryNumbers/Solution.cs
+++ DecibinaryNumbers/DecibinaryNumbers/Solution.cs
@@ -11,16 +11,19 @@ namespace DecibinaryNumbers {
             Solution ans = new Solution();
             for (int a0 = 0; a0 < q; a0++) {
                 long x = Convert.ToInt64(Console.ReadLine());
-                long result = ans.decibinaryNumbers(x + 1);
+                long result = ans.decibinaryNumbers(x);
                 Console.WriteLine(result);
             }
         }
 
         Solution() {
-            long[] t = new long[sz];
-            for (int i = 0; i < t.Length; t[i++] = 1) ;
-            decamt = new List<long[]> { t, t };
-            dist = new List<long> { 1, 2 };
+            decamt = new List<long[]>(5);
+            decamt.Add(new long[32] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+            decamt.Add(new long[32] { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+            decamt.Add(new long[32] { 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 });
+            decamt.Add(new long[32] { 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 });
+            decamt.Add(new long[32] { 0, 1, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 });
+            dist = new List<long> { 1, 2, 4, 6, 10 };
             dgr10 = new List<long> { 1 };
             for (int i = 0; i < 16; dgr10.Add(10 * dgr10[i++])) ;
         }
@@ -30,7 +33,7 @@ namespace DecibinaryNumbers {
         readonly List<long> dgr10;
 
         long decibinaryNumbers(long x) {
-            for( int i = dist.Count; x > dist.Last(); ++ i) {
+            for (int i = dist.Count; x > dist.Last(); ++i) {
                 decamt.Add(FindDecNumCnt(i));
                 dist.Add(decamt[i][31] + dist[i - 1]);
             }
@@ -38,26 +41,28 @@ namespace DecibinaryNumbers {
         }
 
         private long[] FindDecNumCnt(int x) {
-            int k = findPosition(x,0);
+            int k = findPosition(x, 0);
             var cnt = new long[sz];
-            for (int i1 = 0; x - (1 << k - 1) * i1 >= 0;  ++ i1) {
+            for (int i1 = 0; x - (1 << k - 1) * i1 >= 0; ++i1) {
                 int x1 = x - (1 << k - 1) * i1;
                 long cnt1 = 0;
                 for (int i2 = 0; x1 - (1 << k - 2) * i2 >= 0; ++i2) {
                     int x2 = x1 - (1 << k - 2) * i2;
                     long cnt2 = 0;
-                    for (int i3 = 0; x2 - (1 << k - 3) * i3 >= 0; ++i3) {
-                        int x3 = x2 - (1 << k - 3) * i3;
-                        cnt2 += decamt[x3][k - 3];
-                    }
-                    if (i1 + i2 == 0) cnt[k - 2] += cnt2;
+                    if (k >= 3) {
+                        for (int i3 = 0; x2 - (1 << k - 3) * i3 >= 0; ++i3) {
+                            int x3 = x2 - (1 << k - 3) * i3;
+                            cnt2 += decamt[x3][k - 3];
+                        }
+                        if (i1 + i2 == 0) cnt[k - 2] += cnt2;
+                    } else if (x2 == 0) cnt2 = 1;
                     cnt1 += cnt2;
                 }
                 if (i1 == 0) cnt[k - 1] += cnt1;
                 cnt[k] += cnt1;
             }
             cnt[k + 1] = cnt[k] + decamt[x - (1 << k)][k];
-            for (int i = sz - 1; i > k + 1; cnt[i--] = cnt[k + 1]);
+            for (int i = sz - 1; i > k + 1; cnt[i--] = cnt[k + 1]) ;
             return cnt;
         }
 
@@ -65,7 +70,7 @@ namespace DecibinaryNumbers {
             long ans = 0;
             int s = dist.BinarySearch(pos);
             if (s < 0) s = ~s;
-            pos -= dist[s - 1];
+            if( s > 0) pos -= dist[s - 1];
             for (int i = findPosition(s, 0); i >= 0; --i) {
                 int j = 0;
                 for (j = 0; decamt[s - (1 << i) * j][i] < pos; ++j) ;
