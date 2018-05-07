89b25086605304f236c1e6a2492c2a25b07b9ce9
PLDD <PLDD@DK01WV6074>
2018-05-07 20:21:22 +0200

    problem task is completed.

--- DecibinaryNumbers/DecibinaryNumbers/Solution.cs
+++ DecibinaryNumbers/DecibinaryNumbers/Solution.cs
@@ -16,53 +16,45 @@ namespace DecibinaryNumbers {
             }
         }
 
-        Solution() {
-            decamt = new List<long[]>(5);
-            decamt.Add(new long[32] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
-            decamt.Add(new long[32] { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
-            decamt.Add(new long[32] { 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 });
-            decamt.Add(new long[32] { 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 });
-            decamt.Add(new long[32] { 0, 1, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 });
-            dist = new List<long> { 1, 2, 4, 6, 10 };
-            dgr10 = new List<long> { 1 };
-            for (int i = 0; i < 16; dgr10.Add(10 * dgr10[i++])) ;
-        }
-
         List<long[]> decamt;
         List<long> dist;
         readonly List<long> dgr10;
+        readonly List<long> dgr9;
+
+        Solution() {
+            decamt = new List<long[]> { new long[sz] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } };
+            dist = new List<long> { 1 };
+            dgr10 = new List<long> { 1 };
+            dgr9 = new List<long> { 9 };
+            for (int i = 0; i < sz; dgr10.Add(10 * dgr10[i++])) ;
+            for (int i = 1; i < sz; ++ i) dgr9.Add((9 * (1 << i)) + dgr9[i - 1]);
+        }
 
         long decibinaryNumbers(long x) {
             for (int i = dist.Count; x > dist.Last(); ++i) {
-                decamt.Add(FindDecNumCnt(i));
-                dist.Add(decamt[i][31] + dist[i - 1]);
+                int k = findPosition(i, 0);
+                decamt.Add(new long[sz]);
+                dist.Add(FindDecNumCnt(i, i, k) + dist[i - 1]);
+                for (int j = sz - 1; j > k + 1; decamt[i][j--] = decamt[i][k + 1]) ; 
             }
+
             return findDecNum(x);
         }
 
-        private long[] FindDecNumCnt(int x) {
-            int k = findPosition(x, 0);
-            var cnt = new long[sz];
-            for (int i1 = 0; x - (1 << k - 1) * i1 >= 0; ++i1) {
-                int x1 = x - (1 << k - 1) * i1;
-                long cnt1 = 0;
-                for (int i2 = 0; x1 - (1 << k - 2) * i2 >= 0; ++i2) {
-                    int x2 = x1 - (1 << k - 2) * i2;
-                    long cnt2 = 0;
-                    if (k >= 3) {
-                        for (int i3 = 0; x2 - (1 << k - 3) * i3 >= 0; ++i3) {
-                            int x3 = x2 - (1 << k - 3) * i3;
-                            cnt2 += decamt[x3][k - 3];
-                        }
-                        if (i1 + i2 == 0) cnt[k - 2] += cnt2;
-                    } else if (x2 == 0) cnt2 = 1;
-                    cnt1 += cnt2;
-                }
-                if (i1 == 0) cnt[k - 1] += cnt1;
-                cnt[k] += cnt1;
+        long FindDecNumCnt(int x, int y, int pos) {
+            if (pos < 0) {
+                if (y == 0) return 1;
+                return 0;
             }
-            cnt[k + 1] = cnt[k] + decamt[x - (1 << k)][k];
-            for (int i = sz - 1; i > k + 1; cnt[i--] = cnt[k + 1]) ;
+            if (dgr9[pos] < y) return 0;
+            if (x > y) return decamt[y][pos + 1];
+            long cnt = 0;
+            int t = y;
+            for (int i = 0; i <= 9 && t >= 0; ++i) {
+                cnt += FindDecNumCnt(x, t, pos - 1);
+                t -= (1 << pos);
+            }
+            if (x - y == 0) decamt[x][pos + 1] = cnt;
             return cnt;
         }
 
@@ -73,8 +65,11 @@ namespace DecibinaryNumbers {
             if( s > 0) pos -= dist[s - 1];
             for (int i = findPosition(s, 0); i >= 0; --i) {
                 int j = 0;
-                for (j = 0; decamt[s - (1 << i) * j][i] < pos; ++j) ;
-                s -= (1 << i) * j;
+                for (j = 0; decamt[s][i] < pos; ++j) {
+                    pos -= decamt[s][i];
+                    s -= (1 << i);
+                    if (s < 0) break;
+                }
                 ans += j * dgr10[i];
             }
             return ans;
