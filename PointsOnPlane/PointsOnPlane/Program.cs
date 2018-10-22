35236b304c40199884beca44f6a7c16e85a5f5a3
unknown <p.demydiuk@gmail.com>
2018-10-23 00:26:46 +0300

    Second time

--- PointsOnPlane/PointsOnPlane/Program.cs
+++ PointsOnPlane/PointsOnPlane/Program.cs
@@ -4,10 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PointsOnPlane
 {
     class Program
     {
+        static int[] pointsInPlane(int[][] coordinates)
+        {
+            //Comparison<int[]> comp = (x1, x2) => x1[0] < x2[0] ? -1 : (x1[0] == x2[0]) && (x1[1] < x2[1]) ? -1 : 1;
+            int comp(int[] x1, int[] x2) => x1[0] < x2[0] ? -1 : (x1[0] == x2[0]) && (x1[1] < x2[1]) ? -1 : 1;
+            Array.Sort(coordinates, Comparer<int[]>.Create(comp));
+            return new int[2];
+        }
+
         static void Main(string[] args)
         {
         }
