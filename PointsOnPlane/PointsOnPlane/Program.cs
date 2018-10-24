c78b16477d1e59b05bd6b85f0d94655a35f6ff44
unknown <p.demydiuk@gmail.com>
2018-10-24 08:36:54 +0300

    aaaaaa

--- PointsOnPlane/PointsOnPlane/Program.cs
+++ PointsOnPlane/PointsOnPlane/Program.cs
@@ -9,32 +9,88 @@ namespace PointsOnPlane
 {
     class Program
     {
-        static int[] pointsInPlane(int[][] c)
+        public Program()
         {
-            Comparison<int[]> comp = (x1, x2) => x1[0] < x2[0] ? -1 : (x1[0] == x2[0]) && (x1[1] < x2[1]) ? -1 : 1;
-            Array.Sort(c, Comparer<int[]>.Create(comp));
-            int n = c.Length;
-            int[,] dp = new int[1 << n, 2];
-            int[,] sets = new int[n, n];
+            int n = 1 << 16;
+            masks = new int[n][][];
+            for(int i = 1; i < n; ++ i)
+            {
+                List<int[]> values = new List<int[]> { new int[2] { 0, 0 } };
+                int init_pos = -1;
+                for(var j = 0; j < 16; ++ j) {
+                    if(((1 << j) & i) != 0) {
+                        if (init_pos < 0) init_pos = j;
+                        values.Add(new int[2] { init_pos, j });
+                    }                   
+                }
+                masks[i] = values.ToArray();
+            }
+        }
+        private int[,] sets;
+
+        readonly int[][][] masks = null;
+
+
+
+        private Comparison<int[]> comp = (x1, x2) => x1[0] < x2[0] ? -1 : (x1[0] == x2[0]) && (x1[1] < x2[1]) ? -1 : 1;
+        private Func<int[], int[], bool> iscolinear = (p1, p2) => (p1[0] * p2[1] - p1[1] * p2[0]) == 0;
+        private int encodeset(int i, int setid) => setid | (1 << i);
+
+        public void Preprocessing(int[][] points)
+        {
+            Array.Sort(points, Comparer<int[]>.Create(comp));
+            int n = points.Length;
             List<int> setids = new List<int> { 0 };
-            Func<int[], int[], bool> iscolinear = (p1, p2) => (p1[0] * p2[1] - p1[1] * p2[0]) == 0;
-            int encodeset(int i, int setid) => setid | (1 << i);
-            for(int i = 0; i < n; ++i)
+            for (int i = 0; i < n; ++i)
             {
                 setids.Add(encodeset(i, 0));
-                for(int j = i + 1; j < n; ++ j)
+                int baseid = sets[i, i] = setids.Count;
+                for (int j = i + 1; j < n; ++j)
                 {
-                    for(int k = 0;k < j; ++ k)
+                    int[] p1 = new int[2] { points[j][0] - points[i][0], points[j][1] - points[i][1] };
+                    for (int k = 0; k < j; ++k)
                     {
-
+                        if (k == i) continue;
+                        int[] p2 = new int[2] { points[k][0] - points[i][0], points[k][1] - points[i][1] };
+                        int id = sets[i, k];
+                        if (!iscolinear(p1, p2)) {
+                            setids.Add(setids[baseid]);
+                            id = setids.Count;
+                        }
+                        setids[id] = encodeset(j, setids[id]);
+                        sets[i, j] = sets[j, i] = id;
                     }
+                }
+            }
+            for (int i = 0; i < n; ++i)
+                for (int j = 0; j < n; ++j)
+                    sets[i, j] = setids[sets[i, j]];
+        }
+
+        public int[] FindSolution()
+        {
+            int prime = 888888;
+            int n = sets.GetLength(0);
+            int power = 1 << n + 1;
+            int[,] dp = new int[1 << n, 2];
+            for (int i = 1; i < power; ++i)
+            {
+                foreach (var p in masks[i])
+                {
+                    int mask = sets[p[0], p[1]];
+                    int subset = (i ^ mask) & i;
+                    int newpower = 1 + dp[subset, 0];
+                    if (newpower < dp[i, 0])
+                    {
+                        dp[i, 0] = newpower;
+                        dp[i, 1] = (newpower * dp[subset, 1]) % prime;
+                    } else if (newpower == dp[i, 0])
+                        dp[i, 1] = (dp[i, 1] + dp[subset, 1] * newpower) % prime;
+
                 }
 
             }
-
-
-
-            return new int[2];
+            return new int[2]{dp[n, 0],dp[n, 1]};
         }
 
         static void Main(string[] args)
