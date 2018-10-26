5c66d8c7aefa237ff860c871b808c32d4f30d67c
unknown <p.demydiuk@gmail.com>
2018-10-26 07:25:36 +0300

    next iteration

--- PointsOnPlane/PointsOnPlane/Program.cs
+++ PointsOnPlane/PointsOnPlane/Program.cs
@@ -11,11 +11,6 @@ namespace PointsOnPlane
 
         public int[] GetSolution(int[][] points) {
             init_sets(points);
-            int n = init_dp();
-            if(dp[n,0] == 1)
-            {
-                return new int[2] { dp[n, 0], dp[n, 1] };
-            }
             return find_solution();
         }
 
@@ -24,19 +19,6 @@ namespace PointsOnPlane
         private int encodeset(int i, int setid) => setid | (1 << i);
 
         private const int prime = 1000000007;
-
-        private int[,] dp = null;
-        private int init_dp()
-        {
-            int n = 1 << sets.GetLength(1);
-            dp = new int[n, 2];
-            for(int i = 0; i < n; ++ i) { dp[i, 0] = prime; dp[i, 1] = 0; }
-            dp[0, 1] = 1;
-            for (int i = 0; i < sets.GetLength(0); ++i)
-                for (int j = i; j < sets.GetLength(1); ++j)
-                    dp[sets[i, j], 0] = dp[sets[i, j], 1] = 1;
-            return n - 1;
-        }
 
         private int[,] sets = null;
         private void init_sets(int[][] points) {
@@ -59,8 +41,7 @@ namespace PointsOnPlane
                             break;
                         }
                     }
-                    if(id == 0)
-                    {
+                    if(id == 0){
                         setids.Add(encodeset(j, setids[baseid - 1]));
                         id = setids.Count;
                     }
@@ -78,11 +59,11 @@ namespace PointsOnPlane
             int[][][] _masks = new int[n][][];
             for (int i = 1; i < n; ++i) {
                 List<int[]> tmpvals = new List<int[]>();
-                int init_pos = -1;
+                int j0 = -1;
                 for (var j = 0; j < 16; ++j) {
                     if (((1 << j) & i) != 0) {
-                        if (init_pos < 0) init_pos = j;
-                        tmpvals.Add(new int[2] { init_pos, j });
+                        if (j0 < 0) j0 = j;
+                        tmpvals.Add(new int[4] { j0, j,(~((1 << j0)-1))&((1 << j + 1) - 1), (1 << j0) | (1 << j) });
                     }
                 }
                 _masks[i] = tmpvals.ToArray();
@@ -91,38 +72,28 @@ namespace PointsOnPlane
         }
 
         private int[] find_solution() {
-            int power = 1 << sets.GetLength(0);
-            int[,] dp = new int[power, 2];
-            for (int i = 0; i < power; ++i) { dp[i, 0] = prime; dp[i, 1] = 0; }
+            int n = 1 << sets.GetLength(0);
+            int[,] dp = new int[n, 2];
+            for (int i = 1; i < n; ++i) { dp[i, 0] = prime; dp[i, 1] = 0; }
             dp[0, 1] = 1;
-            dp[0, 0] = 0;
-            for (int i = 1; i < power; ++i)
-            {
-                //if (dp[i, 0] < prime) continue;
-                if(i == 7)
-                {
-                    int sss = 99;
-                }
-                foreach (var p in masks[i])
-                {
-                    int mask = sets[p[0], p[1]];
-                    int subset = (i ^ mask) & i;
-                    int newpower = 1 + dp[subset, 0];
-                    if (newpower < dp[i, 0])
-                    {
-                        dp[i, 0] = newpower;
-                        dp[i, 1] = ((newpower) * dp[subset, 1]) % prime;
-                        if (subset == 0) dp[i, 1] = 1;
+            for (int i = 1; i < n; ++i) {
+                foreach (var p in masks[i]){
+                    int mask = p[2] & sets[p[0], p[1]];
+                    int subset = i ^ mask;
+                    int m = 1 + dp[subset, 0];
+                    if (m < dp[i, 0]) {
+                        dp[i, 0] = m;
+                        dp[i, 1] = (m * dp[subset, 1]) % prime;
                     }
-                    else if (newpower == dp[i, 0])
-                    {
-                        int tmp = 1;
-                        if (subset != 0) tmp = (dp[subset, 1] * (newpower)) % prime;
-                        dp[i, 1] = (dp[i, 1] + tmp) % prime;
+                    else if (dp[i, 0] == m) {
+                        dp[i, 1] = (dp[i, 1] + dp[subset, 1] * m) % prime;
+                        int subset2 = p[3] ^ i;
+                        if((subset!= subset2) && (dp[subset2,0] == m - 1))
+                            dp[i, 1] = (dp[i, 1] + dp[subset2, 1] * m) % prime;
                     }
                 }
             }
-            return new int[2]{dp[power - 1, 0],dp[power - 1, 1]};
+            return new int[2]{dp[n - 1, 0],dp[n - 1, 1]};
         }
 
         static void Main(string[] args)
@@ -146,12 +117,12 @@ namespace PointsOnPlane
                 int[] result = res.GetSolution(coordinates);
 
                 Console.WriteLine("{0} {1}", result[0], result[1]);
-
+                //4 168
                 //textWriter.WriteLine(string.Join(" ", result));
             }
 
             ////textWriter.Flush();
-           // textWriter.Close();
+            // textWriter.Close();
         }
     }
 }
