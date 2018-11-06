889097c7e0b1fddf0dcb9f837f377cb7ad3ec79c
Pavlo Demydiuk <PLDD@simcorp.com>
2018-11-06 12:33:44 +0100

    work

---
+++ PointsInPlanes/PointsInPlanes/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution {
+
+
+
+
+    /*
+     * Complete the pointsInPlane function below.
+     */
+    static int[] pointsInPlane(int[][] coordinates) {
+        int n = coordinates.Length;
+        int group = 1;
+        //int[,] lines = new int[n, n];
+        //for (int i = 0; i < n; ++i) {
+        //    int xi = coordinates[i][0];
+        //    int yi = coordinates[i][1];
+        //    for (int j = i + 1; j < n; ++j) {
+        //        int xj = coordinates[j][0] - xi;
+        //        int yj = coordinates[j][1] - yi;
+        //        for(int k = 0; k < j; ++k) {
+        //           var fff =  (coordinates[k][0] - xi) * yj - (coordinates[k][1] - yi) * xj;  
+        //        }
+
+
+
+        //    }
+
+            return new int[10];
+
+    }
+
+    static void Main(string[] args) {
+        int[][] arr =  { new int[]{1,3 }, new int[]{1,1}, new int[]{1,2 },
+                                   new int[]{2,6 }, new int[]{2,1 } };
+        var comp = Comparer<int[]>.Create((x, y) => x[0] < y[0] ? -1 : (x[0] == y[0] && x[1] < y[1]) ? -1 : 1);
+        Array.Sort(arr, comp);
+        int sss = 0;
+
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        //int t = Convert.ToInt32(Console.ReadLine());
+
+        //for (int tItr = 0; tItr < t; tItr++) {
+        //    int n = Convert.ToInt32(Console.ReadLine());
+
+        //    int[][] coordinates = new int[n][];
+
+        //    for (int coordinatesRowItr = 0; coordinatesRowItr < n; coordinatesRowItr++) {
+        //        coordinates[coordinatesRowItr] = Array.ConvertAll(Console.ReadLine().Split(' '), coordinatesTemp => Convert.ToInt32(coordinatesTemp));
+        //    }
+
+        //    int[] result = pointsInPlane(coordinates);
+
+        //    textWriter.WriteLine(string.Join(" ", result));
+        //}
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
---
+++ work/DataSourceAdapterCalculationEngineTest.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SimCorp.IMS.MiddleOffice.Engine.Api;
+using SimCorp.IMS.MiddleOffice.Engine.Api.Common;
+using SimCorp.IMS.MiddleOffice.Engine.Facade;
+using SimCorp.IMS.MiddleOffice.Engine.Facade.Common;
+using SimCorp.IMS.Framework.DataSharing.Service.Implementation;
+using SimCorp.IMS.Framework.DataSharing;
+using SimCorp.IMS.Framework.DataSharing.Queryable;
+
+namespace SimCorp.IMS.MiddleOffice.Engine.Api.Test {
+    [TestClass]
+    public class DataSourceAdapterCalculationEngineTest {
+        public List<Analytics> Analytics = new List<Analytics>();
+        public List<CalculationArgument> CalculationArguments = new List<CalculationArgument>();
+        public Mock<ICalculationEngineFacade> MockCalculationEngineFacade = new Mock<ICalculationEngineFacade>();
+        public List<Results> Results = new List<Results>();
+
+        public DataSourceAdapterCalculationEngineTest() {
+            init();
+            MockCalculationEngineFacade.Setup(x => x.GetSupportedAnalytics()).Returns(() => Analytics);
+        }
+
+        [TestMethod]
+        public void TestMethod1() {
+            var dataSource = new DataSourceAdapterCalculationEngine(MockCalculationEngineFacade.Object);
+            dataSource.Initialize();
+
+            var credentials = new OperationCredentials(Environment.UserName.ToUpper());
+
+            var query1 = new QueryData() {
+                EntityName = "Analytics",
+                //PropertyPaths = new[] { "Id", "Key", "Currency.Name" },
+               // Filter = "Currency.Name in (\"DKK\", \"NOK\") and Key != 0",
+                MaxCount = 1000
+            };
+            OperationData result1 = dataSource.SelectAsync(credentials, query1).Result;
+
+        }
+
+        public void init() {
+            foreach (var id in new[] {"IRR", "AnualIRR"}) {
+                foreach (var formattype in new[] {FormatType.Numeric, FormatType.Amount}) {
+                    foreach (var currencytypename in new[] {"QC", "RC", "PC"}) {
+                        foreach (var periodname in new[] {"Year", "Month"}) {
+                            Analytics.Add(new Analytics {
+                                Id = id, Title = id, FormatType = formattype, TypeName = formattype.ToString(),
+                                CurrencyTypeName = currencytypename, PeriodName = periodname
+                            });
+                        }
+                    }
+                }
+            }
+
+            var Date = new DateTime(2014, 12, 21);
+            var tic = 1;
+            foreach (var id in new[] {"IRR", "AnualIRR"}) {
+                foreach (
+                var groupingcriteria in new[] {GroupingCriteriaEnum.PortfolioKey, GroupingCriteriaEnum.SecurityKey}) {
+                    foreach (var restrictionCriteria in
+                    new[] {
+                        RestrictionCriteriaEnum.AlternativeInvestmentAssetKey, RestrictionCriteriaEnum.PortfolioKey,
+                        RestrictionCriteriaEnum.SecurityKey
+                    }) {
+                        foreach (var periodlength in new[] {1, -1}) {
+                            var calcArg = new CalculationArgument {
+                                AnalysisDates = new[] { Date.AddDays(1) },
+                                Groupings = new[] {groupingcriteria},
+                                AnalyticIds = new[] {id},
+                                PeriodTermLength = tic*periodlength,
+                                PeriodTermUnit = PeriodTermUnitEnum.Years,
+                                RestrictionSet =
+                                    new RestrictionSetEntity {
+                                        Restrictions =
+                                            new[] {
+                                                new RestrictionEntity
+                                                {Criteria = restrictionCriteria, Values = new[] {tic}}
+                                            },
+                                        OnlyIncludeActiveInvestment = true, OnlyIncludeSecuritiesLinkedToAnAsset = true
+                                    }
+                            };
+                            if (calcArg.PeriodTermLength < 0) { calcArg.PeriodTermLength = null; }
+                            if (tic%2 == 0) { calcArg.PeriodTermUnit = null; }
+                            CalculationArguments.Add(calcArg);
+
+                            var result =
+                            new Results(new List<AnalyticsResult> {
+                                new AnalyticsResult
+                                {NodeId = tic.ToString(), AnalyticsId = id, CalculationDate = Date, Value = 3.14*tic}
+                            });
+                            var groupings = new List<GroupingCriteria>
+                            {ApiEnumConverter<GroupingCriteriaEnum, GroupingCriteria>.Convert(groupingcriteria)};
+                            var restrictionSet = calcArg.RestrictionSet.Convert();
+                            if (calcArg.PeriodTermLength.HasValue && calcArg.PeriodTermUnit.HasValue) {
+                                var perTermUnit =
+                                ApiEnumConverter<PeriodTermUnitEnum, PeriodTermUnit>.Convert(
+                                calcArg.PeriodTermUnit.Value);
+                                MockCalculationEngineFacade.Setup(
+                                x =>
+                                x.Calculate(Date.Date, perTermUnit, calcArg.PeriodTermLength.Value,
+                                calcArg.AnalyticIds.ToList(), restrictionSet, groupings)).Returns(() => result);
+                            }
+                            else {
+                                MockCalculationEngineFacade.Setup(
+                                x => x.Calculate(Date.Date, calcArg.AnalyticIds.ToList(), restrictionSet, groupings))
+                                .Returns(() => result);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
