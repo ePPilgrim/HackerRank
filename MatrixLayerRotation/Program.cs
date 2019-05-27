12c471c8c764a32ad974909db24ee3550624b3c2
unknown <p.demydiuk@gmail.com>
2019-05-28 00:19:51 +0300

    second commmmit

--- MatrixLayerRotation/Program.cs
+++ MatrixLayerRotation/Program.cs
@@ -13,23 +13,72 @@ using System.Text;
 using System;
 
 internal class Matrix{
-    public List<List<int>> matrix;
-    public int R;
+    private List<List<int>> matrix;
+    private readonly int R;
+    private readonly int RCnt;
+    private readonly int CCnt;
+
     public Matrix(List<List<int>> matrix, int r){
         this.matrix = matrix;
         R = r;
+        RCnt = matrix.Count();
+        CCnt = matrix[0].Count();
     }
 
-    private IEnumerable<Tuple<int,int>> generateIndexes(int rix, int cix, int minix, int maxix, int dir){
-        var rc = (cix == minix || cix == maxix) ? 0 : 1;
+    public void SetOutlined(int rix, int cix, int level){
+        var list = new List<int>(2048);
+        foreach(var el in getOutlined(level)){
+            list.Add(el);
+        }
+
+        int i = 0;
+        var shiftedix = getShiftedIx(level); 
+        foreach(var ix in generateIndexes(shiftedix.Item1, shiftedix.Item2, level)){
+            matrix[ix.Item1][ix.Item2] = list[++i];
+        }
+    }
+
+    public void Print(){
+        foreach(var row in matrix){
+            foreach(var el in row){
+                Console.Write($"{el} ");
+            }
+            Console.Write("\n");
+        }
+    }
+
+    private Tuple<int,int> getShiftedIx(int level)
+    {
+        int rr = RCnt - 2 * level;
+        int cc = CCnt - 2 * level;
+        int r = R % (rr + cc - 4); 
+        if(r < rr){
+            return new Tuple<int,int>(level + r, level);
+        } else if(r < cc + rr){
+            return new Tuple<int,int>(RCnt - 1 - level, level + r - rr);
+        } else if(r < 2 * rr + cc){
+            return new Tuple<int,int>(level + 2 * rr + cc - r, CCnt - level - 1);
+        }
+        return new Tuple<int,int>(level,level + 2 * (rr + cc) - r);
+    }
+
+    private IEnumerable<int> getOutlined(int level){
+        var list = new List<int>();
+        foreach(var el in generateIndexes(level, level,level)) {
+            yield return matrix[el.Item1][el.Item2];
+        }
+    }
+
+    private IEnumerable<Tuple<int,int>> generateIndexes(int rix, int cix, int level){
+        var rc = (cix == level || cix == CCnt - level - 1) ? 0 : 1;
         var ix = new int[]{rix, cix};
 
         do{
             yield return new Tuple<int,int>(ix[0], ix[1]);
-            if(ix[rc] == minix || ix[rc] == maxix){
+            if(ix[rc] == level || (ix[rc] + level + 1) == ((rc == 1) ? CCnt : RCnt) ){
                 rc = (rc + 1) % 2;
             }
-            ix[rc] += dir;
+            ix[rc] -= 1;
         } while(ix[0] != rix && ix[1] != cix);
     }
 }
