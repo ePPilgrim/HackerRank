acf83b9a3c2eb246fba2ce018934b6c306a453d9
PLDD <p.demydiuk@gmail.com>
2019-11-07 23:19:55 +0200

    First comment

---
+++ BotCleanLarge/BotCleanLarge.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include<list>
+#include <string>
+
+using namespace std;
+
+typedef pair<int,int> Point;
+vector<vector<int>> dists;
+
+
+
+
+int main()
+{
+
+}
+
+class FindShortestWay{
+    vector<vector<int>> Distance;
+    vector<Point> OffPathPoints;
+    vector<Point> OnPathPoints;
+
+    void findConvexHull(Point p, Point endpoint){
+        
+    }
+
+
+
+};
+
+
+
