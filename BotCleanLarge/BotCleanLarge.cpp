6ec115e51f0cfc38346fb72cb860957af5d79096
PLDD <p.demydiuk@gmail.com>
2019-11-08 22:21:03 +0200

    second commit

--- BotCleanLarge/BotCleanLarge.cpp
+++ BotCleanLarge/BotCleanLarge.cpp
@@ -21,13 +21,18 @@ class FindShortestWay{
     list<Point> OffPathPoints;
     list<Point> OnPathPoints;
 
-    void findConvexHull(Point p0, Point endpoint){
+    void findShortestPath(){}
 
+    void findConvexHull(Point p0, Point endpoint){
         auto it1 = OffPathPoints.begin();
         for(auto it = OffPathPoints.begin(); it != OffPathPoints.end(); ++ it ){
-            auto sign = (it1->first - p0.first) * (it->second - p0.second) - 
-                        (it1->second - p0.second) * (it->first - p0.first);
-            if(sign > 0) it1 = it;
+            int x1 = (it1->first - p0.first);
+            int y1 = (it1->second - p0.second);
+            int x2 = (it->first - p0.first);
+            int y2 = (it->second - p0.second);
+            auto sign = x1 * y2 - y1 * x2;
+            if(sign > 0 || 
+                ((sign == 0) && ((x1 * x1 + y1 * y1) > (x2 * x2 + y2 * y2)))) it1 = it;
         }
         if(*it1 == endpoint) return;
         OnPathPoints.push_back(*it1);
