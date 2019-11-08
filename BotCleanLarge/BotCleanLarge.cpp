ce000ca9cf8a99f27ca29f8655ddcb9a7f292189
PLDD <p.demydiuk@gmail.com>
2019-11-08 22:05:53 +0200

    second commit

--- BotCleanLarge/BotCleanLarge.cpp
+++ BotCleanLarge/BotCleanLarge.cpp
@@ -18,15 +18,21 @@ int main()
 
 class FindShortestWay{
     vector<vector<int>> Distance;
-    vector<Point> OffPathPoints;
-    vector<Point> OnPathPoints;
+    list<Point> OffPathPoints;
+    list<Point> OnPathPoints;
 
-    void findConvexHull(Point p, Point endpoint){
-        
+    void findConvexHull(Point p0, Point endpoint){
+
+        auto it1 = OffPathPoints.begin();
+        for(auto it = OffPathPoints.begin(); it != OffPathPoints.end(); ++ it ){
+            auto sign = (it1->first - p0.first) * (it->second - p0.second) - 
+                        (it1->second - p0.second) * (it->first - p0.first);
+            if(sign > 0) it1 = it;
+        }
+        if(*it1 == endpoint) return;
+        OnPathPoints.push_back(*it1);
+        OffPathPoints.erase(it1);
     }
-
-
-
 };
 
 
