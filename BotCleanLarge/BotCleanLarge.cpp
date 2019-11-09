a3d27811049a621a71fc37e52d0f4a4c65231b77
PLDD <p.demydiuk@gmail.com>
2019-11-09 17:22:59 +0200

    third changes

--- BotCleanLarge/BotCleanLarge.cpp
+++ BotCleanLarge/BotCleanLarge.cpp
@@ -18,25 +18,30 @@ int main()
 
 class FindShortestWay{
     vector<vector<int>> Distance;
-    list<Point> OffPathPoints;
-    list<Point> OnPathPoints;
+    vector<Point> Points;
+    list<int> OffPathPoints;
+    list<int> OnPathPoints;
 
     void findShortestPath(){}
 
-    void findConvexHull(Point p0, Point endpoint){
+    bool findConvexHull(){
+        auto id0 = OnPathPoints.back();
+        auto p0 = Points[id0];
         auto it1 = OffPathPoints.begin();
         for(auto it = OffPathPoints.begin(); it != OffPathPoints.end(); ++ it ){
-            int x1 = (it1->first - p0.first);
-            int y1 = (it1->second - p0.second);
-            int x2 = (it->first - p0.first);
-            int y2 = (it->second - p0.second);
+            int x1 = (Points[*it1].first - p0.first);
+            int y1 = (Points[*it1].second - p0.second);
+            int x2 = (Points[*it].first - p0.first);
+            int y2 = (Points[*it].second - p0.second);
             auto sign = x1 * y2 - y1 * x2;
+            
             if(sign > 0 || 
-                ((sign == 0) && ((x1 * x1 + y1 * y1) > (x2 * x2 + y2 * y2)))) it1 = it;
+                ((sign == 0) && (Distance[id0][*it1] > Distance[id0][*it]))) it1 = it;
         }
-        if(*it1 == endpoint) return;
+        if(*it1 == OnPathPoints.front()) return true;
         OnPathPoints.push_back(*it1);
         OffPathPoints.erase(it1);
+        return false;
     }
 };
 
